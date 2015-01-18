e9218bbd33255fc32e072c726ac96c92cf790f3e
chasetrox <chasetrox@gmail.com>
2015-01-18 18:09:41 -0500

    Added syncing def

--- Player.h
+++ Player.h
@@ -15,9 +15,10 @@ public:
 	void look_at_player_hand(string player_id); //displays ascii art of a
 	                                             //requested player's cards
 	bool split_hand(); //specific behavior for double card hands
+	void sync_holdings(int winnings); //resets player for new turn, adds winnings
 private:
 	int chips; // player's "money"
 	int bet; // player's bet on a single hand
 	Hand *cards[]; // player's cards
 	void print_cards(string player_id); // ascii card printer
-}
+};
