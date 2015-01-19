c08cf547dafc93fcd0e99bd476b1dc00c3e56aac
Shree33 <skbhanderi3@gmail.com>
2015-01-19 16:47:37 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,3 +2,41 @@
 //
 //
 //
+//
+//
+//
+//
+
+
+#include <iostream>
+#include "player.h"
+
+Player::Player()
+{
+	chips = 500;
+	bet = 0;
+}
+
+Player::~Player(){
+	// intentionally left blank
+}
+
+void Player::bet(int amount){
+	// Check if has player has enough
+	chips = chips - amount;
+	bet = amount;
+
+}
+
+
+void Player::look_at_player_hand(string player_id){
+
+}
+
+bool Player::split_hand(){
+
+}
+
+void Player::print_cards(string player_id){
+
+}
