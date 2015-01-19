784436b51b2d5d62f9c772312170e325800bbaaa
Shree33 <skbhanderi3@gmail.com>
2015-01-19 17:27:11 -0500

    constructor with name

--- Player.cpp
+++ Player.cpp
@@ -12,16 +12,36 @@ using namespace std;
 
 Player::Player()
 {
+	cards = new Card[INITIAL_CAPACITY];
 	chips = 500;
 	bet = 0;
 	cards_held = 0;
+	player_name = "default";
 }
+
+Player::Player(string name)
+{
+	cards = new Card[INITIAL_CAPACITY];
+	chips = 500;
+	bet = 0;
+	cards_held = 0;
+	player_name = name;
+}
+
 
 Player::~Player(){
 	delete [] cards;
 }
 
 void Player::bet(int amount){
+	// Check if has player has enough
+	chips -= amount;
+	bet = amount;
+}
+
+
+Card* Player::look_at_player_hand(string player_id) {
+	return &cards;
 	// Check if has player has enough and is within bet min/max
 	if(amount > MINIMUM_BET && amount < MAXIMUM_BET){
 
@@ -41,13 +61,20 @@ Card* Player::look_at_player_hand(string player_id){
 bool Player::split_hand(){
 	// possibly create a new player, both players share a chips, and winnings 
 	// are transferred back to player one
-	
 }
 
 void Player::print_cards(string player_id){
 
+	//print ascii art of cards to player; call look at player hand, convert
+	//that into appropriate art
 }
 
-void sync_holdings(){
+void sync_holdings(int winnings){
+	bet += winnings;
+	chips += bet; //bet+winnings added to holdings
+
+}
+
+void give_cards(Card given_card){
 
 }
